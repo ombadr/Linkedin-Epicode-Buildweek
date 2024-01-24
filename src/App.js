38abf97a369e860a22f523dfd818926d605b216b
giuseppe <giuseppeforce36@gmail.com>
2024-01-24 14:50:06 +0100

    solo posts

--- src/App.js
+++ src/App.js
@@ -1,61 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Profile from './components/Profile';
-import Fetchprofilo from './components/Fetchprofilo';
-
-import { Container, Row, Col } from 'react-bootstrap';
-import MyNavbar from './components/MyNavbar';
-import Footer from './components/Footer';
-import ProfileRightBar from './components/ProfileRightBar';
-
-import Analisi from './components/Analisi';
-import Risorse from "./components/Risorse";
-import Attivita from "./components/Attivita";
-import Formazione from "./components/Formazione";
-import Interessi from "./components/Interessi";
-import ModificaProfilo from "./components/ModificaProfilo";
-import SideBar from './components/SideBar';
-import ProfiliRandom from './components/ProfiliRanom';
-import JobsPage from './components/JobsPage';
-import ProfilePage from './components/ProfilePage';
-
-import Posts from './components/Posts';
+import Posts from './components/Posts/Posts';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Container fluid className='bg-white sticky-navbar'>
-          <Container className=" bg-white">
-            <Row>
-              <Col md={12}>
-                <MyNavbar />
-              </Col>
-            </Row>
-          </Container>
-        </Container>
-        <div style={{ backgroundColor: '#f4f2ee' }}>
-          <Routes>
-            <Route path="/:id" element={<ProfilePage />} />
-            <Route path="/jobs" element={<JobsPage />} />
-          </Routes>
 
-          <Container >
-            <Row className='mt-5'>
-              <Col col={12}>
-                <Footer />
-              </Col>
-            </Row>
-            <hr />
             <Posts />
-          </Container>
-        </div>
-      </div>
-
-    </BrowserRouter>
+      
 
 
   );
