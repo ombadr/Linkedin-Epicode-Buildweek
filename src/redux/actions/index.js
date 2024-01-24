1b0baf411a0a4eafec54919efe71584600e83b74
MattiaBaietta <mattia.baietta89@gmail.com>
2024-01-24 15:45:30 +0100

    fix

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,13 +1,8 @@
-export const FETCH_PROFILES_REQUEST = 'FETCH_PROFILES_REQUEST';
-export const FETCH_PROFILES_SUCCESS = 'FETCH_PROFILES_SUCCESS';
-export const FETCH_PROFILES_FAILURE = 'FETCH_PROFILES_FAILURE';
+
 export const GET_JOBS_BY_CATEGORY = 'GET_JOBS_BY_CATEGORY';
 export const RECENT_SEARCHES = 'RECENT_SEARCHES';
 export const PERFORM_SEARCH = 'PERFORM_SEARCH';
 export const RESET_SEARCH = 'RESET_SEARCH';
-export const FETCH_POSTS_REQUEST = 'FETCH_POSTS_REQUEST';
-export const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS';
-export const FETCH_POSTS_FAILURE = 'FETCH_POSTS_FAILURE';
 export const MAIN_PROFILE = "MAIN_PROFILE";
 export const GET_JOBS_FROM_SEARCH = "GET_JOBS_FROM_SEARCH";
 
@@ -40,46 +35,8 @@ export const mainProfileAction = () => {
   }
 }
 
-export const fetchProfiles = () => {
-  return async (dispatch) => {
-    try {
-      const response = await fetch('https://striveschool-api.herokuapp.com/api/profile/', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFlMzE0MTYwMGJlMTAwMTgzYTg2OGIiLCJpYXQiOjE3MDU5MTQ2ODksImV4cCI6MTcwNzEyNDI4OX0.4wuc8BPQtnbrrjR2fr4os_GS-UinPRJDLkLLihyMLtE`
-        }
-      });
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      const data = await response.json();
-      dispatch({ type: FETCH_PROFILES_SUCCESS, payload: data });
-    } catch (error) {
-      dispatch({ type: FETCH_PROFILES_FAILURE, payload: error.message });
-    }
-  };
-};
-export const fetchPosts = () => {
-  return async (dispatch) => {
-    try {
-      const response = await fetch('https://striveschool-api.herokuapp.com/api/posts/', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFlMzE0MTYwMGJlMTAwMTgzYTg2OGIiLCJpYXQiOjE3MDU5MTQ2ODksImV4cCI6MTcwNzEyNDI4OX0.4wuc8BPQtnbrrjR2fr4os_GS-UinPRJDLkLLihyMLtE`
-        }
-      });
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      const data = await response.json();
-      dispatch({ type: FETCH_POSTS_SUCCESS, payload: data });
-    } catch (error) {
-      dispatch({ type: FETCH_POSTS_FAILURE, payload: error.message });
-    }
-  };
-};
+
+
 
 
 export const getJobsByCategoryAction = (category) => {
