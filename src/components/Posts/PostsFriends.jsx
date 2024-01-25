82d0b9d767dad234a07766391b015518048633b6
giuseppe <giuseppeforce36@gmail.com>
2024-01-25 12:27:00 +0100

    FIXED GG

--- src/components/Posts/PostsFriends.jsx
+++ src/components/Posts/PostsFriends.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
-
 import { GrLike } from 'react-icons/gr';
 import { FaRegCommentDots } from 'react-icons/fa';
 import { IoIosSend } from 'react-icons/io';
 import { RiRepeatLine } from 'react-icons/ri';
 import './assets/Posts.css';
-import { ChatDots } from 'react-bootstrap-icons';
-import PostsComments from './PostsComments'; //
+import PostsComments from './PostsComments';
 
 const PostsFriends = ({ posts }) => {
   const friendUserIds = [
@@ -18,8 +16,7 @@ const PostsFriends = ({ posts }) => {
   ];
 
   const [friendPosts, setFriendPosts] = useState([]);
-  const [selectedPostId, setSelectedPostId] = useState(null); // Nuovo stato per tracciare l'ID del post selezionato
-  const [showModal, setShowModal] = useState(false);
+  const [selectedPostId, setSelectedPostId] = useState(null);
 
   useEffect(() => {
     const filteredFriendPosts = posts.filter((post) =>
@@ -30,11 +27,10 @@ const PostsFriends = ({ posts }) => {
 
   const handleChatIconClick = (postId) => {
     setSelectedPostId(postId);
-    setShowModal(true);
   };
 
   const handleClose = () => {
-    setShowModal(false);
+    setSelectedPostId(null);
   };
 
   return (
@@ -63,7 +59,7 @@ const PostsFriends = ({ posts }) => {
                 <GrLike size={30} className='me-2' />
                 Consiglia
               </button>
-              <button className='mt-2 fs-5 btn btn-post' onClick={() => handleChatIconClick(post._id)}>
+              <button className='mt-2 fs-5 btn btn-post' onClick={() => { handleClose(); handleChatIconClick(post._id);  }}>
                 <FaRegCommentDots size={30} className='me-2' />
                 Commenta
               </button>
@@ -76,18 +72,17 @@ const PostsFriends = ({ posts }) => {
                 Invia
               </button>
             </div>
+
+            {selectedPostId === post._id && (
+              <PostsComments
+                postId={selectedPostId}
+                showModal={true}  // Mostra sempre il componente PostsComments quando selectedPostId è definito
+                handleClose={handleClose}
+              />
+            )}
           </div>
         ))}
       </div>
-
-      {/* Visualizza PostsComments solo se un post è stato selezionato */}
-      {selectedPostId && (
-        <PostsComments
-          postId={selectedPostId}
-          showModal={showModal}
-          handleClose={handleClose}
-        />
-      )}
     </div>
   );
 };
--- src/components/Posts/PostsRandom.jsx
+++ src/components/Posts/PostsRandom.jsx
@@ -12,8 +12,17 @@ const PostsRandom = ({ posts }) => {
   const [showModal, setShowModal] = useState(false);
   const [randomPosts, setRandomPosts] = useState([]);
 
+  const handleChatIconClick = (postId) => {
+    setSelectedPostId(postId);
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+    setSelectedPostId(null);
+  };
+
   useEffect(() => {
-    // Aggiorna la lista dei post random solo al montaggio del componente
     if (posts && posts.length >= 10) {
       const availableIndices = Array.from(
         { length: posts.length },
@@ -30,51 +39,58 @@ const PostsRandom = ({ posts }) => {
           const formattedDate = new Date(
             randomPost.updatedAt
           ).toLocaleDateString();
-          newRandomPosts.push(
-            <div
-              key={i}
-              className='Suggestedposts d-flex p-4 m-3 border-bottom border-secondary bg-light w-100 rounded-4 p-3 border border-secondary mt-4 mb-4'
-            >
-              <div className='w-100'>
-                <h4 className='fw-bold mb-4'>
-                  <img
-                    src={
-                      randomPost.user.image
-                        ? randomPost.user.image
-                        : 'https://d.newsweek.com/en/full/2270410/angry-cat-expert.png?w=1600&h=1600&q=88&f=aeb99a4ed1e4f5223fb24f0610a3493a'
-                    }
-                    alt=''
-                    style={{ width: '100px', height: 'auto' }}
-                    className='rounded-circle me-3'
-                  />
-                  {randomPost.user.username}
-                </h4>
-                <p className=''>{randomPost.text}</p>
-                <p className=''>{formattedDate}</p>
+          newRandomPosts.push({
+            id: randomPost._id,
+            jsx: (
+              <div
+                key={i}
+                className='d-flex p-4 m- w-100 mt-4 mb-4'
+              >
+                <div className='w-100'>
+                  <h4 className='fw-bold mb-4'>
+                    <img
+                      src={
+                        randomPost.user.image
+                          ? randomPost.user.image
+                          : 'https://d.newsweek.com/en/full/2270410/angry-cat-expert.png?w=1600&h=1600&q=88&f=aeb99a4ed1e4f5223fb24f0610a3493a'
+                      }
+                      alt=''
+                      style={{ width: '100px', height: 'auto' }}
+                      className='rounded-circle me-3'
+                    />
+                    {randomPost.user.username}
+                  </h4>
+                  <p className=''>{randomPost.text}</p>
+                  <p className=''>{formattedDate}</p>
 
-                <hr />
+                  <hr />
 
-                <div className='d-flex justify-content-around align-items-center'>
-                  <button className='mt-2 fs-5 btn btn-post'>
-                    <GrLike size={30} className='me-2' />
-                    Consiglia
-                  </button>
-                  <button className='mt-2 fs-5 btn btn-post' onClick={() => handleChatIconClick(randomPost._id)}>
-                    <FaRegCommentDots size={30} className='me-2' />
-                    Commenta
-                  </button>
-                  <button className='mt-2 fs-5 btn btn-post'>
-                    <RiRepeatLine size={30} className='me-2' />
-                    Diffondi il post
-                  </button>
-                  <button className='mt-2 fs-5 btn btn-post'>
-                    <IoIosSend size={30} className='me-2' />
-                    Invia
-                  </button>
+                  <div className='d-flex justify-content-around align-items-center'>
+                    <button className='mt-2 fs-5 btn btn-post'>
+                      <GrLike size={30} className='me-2' />
+                      Consiglia
+                    </button>
+                    <button
+                      className='mt-2 fs-5 btn btn-post'
+                      onClick={() => handleChatIconClick(randomPost._id)}
+                    >
+                      <FaRegCommentDots size={30} className='me-2' />
+                      Commenta
+                    </button>
+                    <button className='mt-2 fs-5 btn btn-post'>
+                      <RiRepeatLine size={30} className='me-2' />
+                      Diffondi il post
+                    </button>
+                    <button className='mt-2 fs-5 btn btn-post'>
+                      <IoIosSend size={30} className='me-2' />
+                      Invia
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
+            ),
+          });
+
           availableIndices.splice(randomIndex, 1);
         } else {
           i--;
@@ -85,27 +101,21 @@ const PostsRandom = ({ posts }) => {
     }
   }, [posts]);
 
-  const handleChatIconClick = (postId) => {
-    setSelectedPostId(postId);
-    setShowModal(true);
-  };
-
-  const handleClose = () => {
-    setShowModal(false);
-  };
-
   return (
     <div>
-      {randomPosts}
-      {selectedPostId && (
-        <PostsComments
-          postId={selectedPostId}
-          showModal={showModal}
-          handleClose={handleClose}
-        />
-      )}
+      {randomPosts.map(({ id, jsx }) => (
+        <div className='Suggestedposts p-4 m-3 border-bottom border-secondary bg-light w-100 rounded-4 p-3 border border-secondary mt-4 mb-4' key={id}>
+          <React.Fragment>
+            {jsx}
+            {selectedPostId === id && showModal && (
+              <PostsComments postId={selectedPostId} showModal={showModal} handleClose={handleClose} />
+            )}
+          </React.Fragment>
+        </div>
+      ))}
     </div>
   );
+  
 };
 
 export default PostsRandom;
